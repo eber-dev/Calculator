a30caaab5e8fe57f52d4b93e7bb438e81213de1a
Eber <www.eberacunaquispe@gmail.com>
2026-02-23 14:56:10 -0500

    Se arreglo el poblema de los decimales tomando como eje el operador si es null trabajamos con el num1 y si no es null trabajamos con el num2

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
 
-function substract(a,b){
-    return a-b
+function substract(a, b) {
+    return a - b
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
 
-function divide(a,b){
-    if(b===0){
+function divide(a, b) {
+    if (b === 0) {
         return undefined
-    }else{
-        return a/b
+    } else {
+        return a / b
     }
 }
 
-function porcent(a){
-    return a*0.01
+function porcent(a) {
+    return a * 0.01
 }
 
-let num1="",num2="",operator=null,respuesta=null;
+let num1 = "", num2 = "", operator = null, respuesta = null;
 
-function operate(num1,operator,num2){
+function operate(num1, operator, num2) {
     let resultado
-    switch(operator){
+    switch (operator) {
         case "+":
-            resultado = add(num1,num2)
+            resultado = add(num1, num2)
             break;
         case "-":
-            resultado = substract(num1,num2)
+            resultado = substract(num1, num2)
             break;
         case "*":
-            resultado = multiply(num1,num2)
+            resultado = multiply(num1, num2)
             break;
         case "/":
-            resultado = divide(num1,num2)
+            resultado = divide(num1, num2)
             break;
         case "%":
             resultado = porcent(num1)
@@ -53,56 +53,73 @@ const display = document.querySelector(".display")
 const teclado = document.querySelector(".teclas")
 const botones = document.querySelectorAll("button")
 
-botones.forEach((p)=>{
-    p.addEventListener("click",(e)=>{
+botones.forEach((p) => {
+    p.addEventListener("click", (e) => {
         let valor = e.target.value;
-        let numerico =["1","2","3","4","5","6","7","8","9","0"].includes(valor) 
-        let operadores = ["+","-","*","/","%"].includes(valor)
-        let decimal = valor==="."
-        let masmenos = valor==="signo"
-        let igualdad = valor==="="
+        let numerico = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].includes(valor)
+        let operadores = ["+", "-", "*", "/", "%"].includes(valor)
+        let decimal = valor === "."
+        let masmenos = valor === "signo"
+        let igualdad = valor === "="
 
-        if(numerico){
-            if(operator===null && num2 === ""){
-                num1+=valor
-            }else if(operator!=null && num1 !==""){
-                num2+=valor
+        if (numerico) {
+            if (operator === null && num2 === "") {
+                num1 += valor
+            } else if (operator !== null && num1 !== "") {
+                num2 += valor
             }
         }
-        
-        if(operadores){
-            if(num1===""){
+
+        if (operadores) {
+            if (num1 === "") {
                 console.log("Debe ingresar un numero primero")
                 return
-            }else if(num1 === "0."){
+            } else if (num1 === "0.") {
                 console.log("Formato Invalido")
-            }else{
+            } else {
                 operator = valor
             }
         }
 
-        if(decimal){
+        if (decimal) {
+            if(operator===null){
+                if(num1===""){
+                    num1 = "0."
+                }else if(num1.includes(".")){
+                    return
+                }else{
+                    num1+=valor
+                }
+            }else{
+                if(num2===""){
+                    num2="0."
+                }else if(num2.includes(".")){
+                    return
+                }else{
+                    num2+=valor
+                }
+            }
 
         }
 
-        if(masmenos){
+        if (masmenos) {
 
         }
 
-        if(igualdad){
-            if(num1==="" || operator===null || num2===""){
+        if (igualdad) {
+            if (num1 === "" || operator === null || num2 === "") {
                 console.log("Formato invalido")
-            }else if(num1 != "" && num2 != "" && operator !=null){
+            } else if (num1 != "" && num2 != "" && operator != null) {
 
-            }else if(resultado!=null && num2 != "" && operator != null){
+            } else if (resultado != null && num2 != "" && operator != null) {
 
             }
-            
+
         }
 
-        console.log("a: ",num1)
-        console.log("operator",operator)
-        console.log("b: ",num2)
+        console.log("a: ", num1)
+        console.log("operator", operator)
+        console.log("b: ", num2)
 
     })
 })
